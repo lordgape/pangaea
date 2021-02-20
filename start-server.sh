95cdcd39efcad0efef0cde868d929799bc8cb87d
Chesvic Lordgape <lordgape@gmail.com>
2021-02-20 23:43:01 +0100

    Rearrage folders

--- start-server.sh
+++ start-server.sh
@@ -2,16 +2,18 @@
 # set -x
 # set -e
 
-function boot_up() {
-    docker-compose up
-}
 
-function update_repos() {
-    mr update
+
+function boot_up() {
+    mkdir www
+    (cd www && git clone 'https://github.com/lordgape/parrot.git')    
+    (cd www && git clone 'https://github.com/lordgape/minion.git')
+    (cd www && git clone 'https://github.com/lordgape/consumer.git')
+    (cd www && git clone 'https://github.com/lordgape/dock.git')
+    (cd www/dock && docker-compose up)
 }
 
 if [ -z $@ ]; then
-    update_repos
     boot_up
 fi
 
